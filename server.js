0a81eab917b9acd2512497e76b6b818489d25d03
Sergio Di Martino <sergio@webdevserg.io>
2019-03-22 21:49:55 -0700

    Begins adding layout

--- server.js
+++ server.js
@@ -15,5 +15,5 @@ var routes = require("./controllers/burgers_controller.js");
 app.use(routes);
 
 app.listen(PORT, function() {
-  console.log("Listening on port:%s", PORT);
+  console.log("Server listening on: http://localhost:" + PORT);
 });
