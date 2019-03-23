c0e54ff9357a7e21eb793319bd883b744953d87c
Sergio Di Martino <sergio@webdevserg.io>
2019-03-23 14:39:51 -0700

    Adds styling

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var express = require("express");
 var PORT = process.env.PORT || 3000
 var app = express();
+var path = require("path");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
 var exphbs = require("express-handlebars");
 
