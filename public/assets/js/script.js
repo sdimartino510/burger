3f8ad96bc6251a78d39a67aad7fdaa4a0bb34cc7
Sergio Di Martino <sergio@webdevserg.io>
2019-03-23 16:42:00 -0700

    Adds form script and additional styling

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
-    
+    $("#submit").on("click", function() {
+        event.preventDefault();
+
+        var newBurger = $("#burger").val().trim();
+        console.log(newBurger);
+
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(
+            function() {
+                //console.log("created new cat");
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    })
 })
